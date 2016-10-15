02074cd2d97b551589abed7056f323770537c6cd
puranzhang <puranzhang@gmail.com>
2016-10-14 21:29:17 -0400

    write msg logs to .csv file

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+//require fast-csv to write msg file
+var csv = require("fast-csv");
+//this array store arrayS of timestamp and msg
+var msgData = [];
+//specify output .csv file directory
+var PATH = 'data.csv';
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,12 +14,17 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    //print timestamp and message on consle
-    console.log(new Date() + ' message: ' + msg);
+  	//append timestamp and msg to array
+  	msgData.push([new Date()]);
+  	msgData.push([msg]);
+
+  	//write out msg logs
+  	csv.writeToPath(PATH, msgData, {headers : false});
+
     io.emit('chat message', msg);
   });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:' + '3000');
 });
